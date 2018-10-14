04c317769cce971281fee6a74e1591b2471b6903
de MOURGUES Jérôme <jerome@demourgues.com>
2018-10-14 22:39:57 +0200

    ex26 committed

---
+++ Piscine_reloaded/ex26/ft_count_if.c
@@ -0,0 +1,17 @@
+
+
+int ft_count_if(char **tab, int(*f)(char*))
+{
+	int i;
+	int nb;
+
+	i = 0;
+	nb = 0;
+	while (tab[i][0] != '0')
+	{	
+		if (f(tab[i] == 1)
+			nb++;
+		i++;
+	}
+	return (nb);
+}
