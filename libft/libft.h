73e48f6a57f264cb741a436b559db3258e2f336d
Jerome de MOURGUES <jde-mour@student.le-101.fr>
2018-10-26 15:58:21 +0200

    ft_strncmp.c committed

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jde-mour <jerome@demourgues.com>           +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/15 16:14:44 by jde-mour     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/23 09:36:28 by jde-mour    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/26 15:24:26 by jde-mour    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -22,7 +22,6 @@
 
 int		ft_strlen(char *str);
 char	*ft_strdup(char *src);
-int		ft_strcmp(char *s0, char *s2);
 void	ft_putchar(char c);
 void	ft_putnbr(int nb);
 void	ft_putstr(char *str);
@@ -35,5 +34,7 @@ char	*ft_strchr(const char *src, int c);
 char	*ft_strrchr(const char *src, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_strcmp(char *s0, char *s2);
+int		ft_strncmp(char *s0, char *s2, size_t n);
 
 #endif
