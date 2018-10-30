b37b77a8055631c22a7ae0abf7f293588eed23bc
Jerome de MOURGUES <jde-mour@student.le-101.fr>
2018-10-30 09:25:32 +0100

    ft_isalnum.c committed

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jde-mour <jerome@demourgues.com>           +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/15 16:14:44 by jde-mour     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/30 08:59:46 by jde-mour    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/30 09:23:27 by jde-mour    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -39,5 +39,6 @@ int		ft_strcmp(char *s0, char *s2);
 int		ft_strncmp(char *s0, char *s2, size_t n);
 int		ft_atoi(const char *nptr);
 int		ft_isalpha(int c);
-
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
 #endif
