01f0280d32ade428cd1e0ffed2d24b0950971929
Jerome de MOURGUES <jde-mour@student.le-101.fr>
2018-10-31 20:16:53 +0100

    ft_memcpy.c changed

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jde-mour <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/31 18:02:22 by jde-mour     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/31 18:20:16 by jde-mour    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/31 20:09:48 by jde-mour    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,7 +15,13 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	while (n--)
-		((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
+	size_t i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
 	return (dest);
 }
