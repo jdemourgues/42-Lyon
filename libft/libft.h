392bbefacc1c18b4a7f54edbf1d2acd76adcae5a
Jerome de MOURGUES <jde-mour@student.le-101.fr>
2018-10-31 18:21:42 +0100

    ft_memcpy.c committed

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jde-mour <jerome@demourgues.com>           +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/15 16:14:44 by jde-mour     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/30 18:06:20 by jde-mour    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/31 18:20:20 by jde-mour    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -21,6 +21,8 @@
 # include <xlocale.h>
 # include <ctype.h>
 
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *sr1, const void *sr2, size_t n);
 int		ft_strlen(char *str);
