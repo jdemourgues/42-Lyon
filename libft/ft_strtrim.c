e39d4374f3191a58645d7e477d5e8dcc8036f362
Jerome de MOURGUES <jde-mour@student.le-101.fr>
2018-11-19 17:35:19 +0100

    ft_strtrim on air

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jde-mour <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/14 10:39:33 by jde-mour     #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/14 10:41:04 by jde-mour    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/19 17:32:10 by jde-mour    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,5 +15,14 @@
 
 char	*ft_strtrim(char const *s)
 {
+	int	start;
+	int end;
 
-}
+	start = 0;
+	end = ft_strlen((char *)s);
+	while (ft_isblank(s[start]))
+		start++;
+	while (ft_isblank(s[end]))
+		end--;
+	return(ft_strsub(s, start, end - start));
+}	
