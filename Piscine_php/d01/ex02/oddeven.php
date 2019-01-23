e77fdf69f5ffb47685e30b791fe5107d8ad7d94a
Jerome de Mourgues <jerome@demourgues.com>
2019-01-23 23:37:43 +0100

    ex02 fixed

--- Piscine_php/d01/ex02/oddeven.php
+++ Piscine_php/d01/ex02/oddeven.php
@@ -4,17 +4,21 @@
 	while ($stdin && !feof($stdin))
 	{
 		echo "Entrez un nombre :";
-		$input = trim(fgets($stdin));
-		if (is_numeric($input))
-        {
-			if ($input % 2 == 0)
-				echo "Le chiffre ". $input . " est Pair\n";
-			else
-				echo "Le chiffre ". $input ." est Impair\n";
+		if($input = trim(fgets($stdin)))
+            {
+		    if (is_numeric($input))
+            {
+			    if ($input % 2 == 0)
+				    echo "Le chiffre ". $input . " est Pair\n";
+			    else
+				    echo "Le chiffre ". $input ." est Impair\n";
+            }
+		    else
+			    echo "'". $input ."'" . " n'est pas un chiffre\n";
+	    }
+		else {
+            echo "^D\n";
+            break;
         }
-		else
-			echo "'". $input ."'" . " n'est pas un chiffre\n";
 	}
-	fclose ($stdin);
-	echo "\n";
 ?>
