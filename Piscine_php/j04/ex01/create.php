abd9241f44157d7bb61dc74b047615f004b787a7
Jerome de Mourgues <jerome@demourgues.com>
2019-01-26 21:17:35 +0100

    ex01 initial commit

--- Piscine_php/j04/ex01/create.php
+++ Piscine_php/j04/ex01/create.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jde-mour
- * Date: 2019-01-26
- * Time: 20:18
- */
+    if (!$_POST["login"] === "" || $_POST["passwd"] === "" || $_POST["submit"] !== "OK") {
+        echo "ERROR\n";
+        return ;
+    }
+
+    if (!file_exists("../private") || !file_exists("../private/passwd"))
+            mkdir ("../private");
+
+    if (file_exists("../private/passwd")) {
+        $array_user = unserialize(file_get_contents("../private/passwd"));
+        foreach ($array_user as $user) {
+            if ($user["login"] === $_POST["login"]) {
+                echo "ERROR\n";
+                return;
+            }
+        }
+    }
+
+    $my_user["login"] = $_POST["login"];
+    $my_user["passwd"] = hash('whirlpool', $_POST["passwd"]);
+    $array_user[] = $my_user;
+    file_put_contents("../private/passwd", serialize($array_user));
+    echo "OK\n";
+?>
