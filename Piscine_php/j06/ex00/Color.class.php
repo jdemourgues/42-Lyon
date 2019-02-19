60ca00076f3bba998f294b927fdbd7b459868906
Jerome de Mourgues <jerome@demourgues.com>
2019-02-19 19:16:13 +0100

    piscine php updated

--- Piscine_php/j06/ex00/Color.class.php
+++ Piscine_php/j06/ex00/Color.class.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jde-mour
- * Date: 2019-01-30
- * Time: 19:35
- */
+class Color
+{
+    public $_red;
+    public $_green;
+    public $_blue;
+    public $_rgb;
+
+    public function __construct($_red, $_green, $_blue, $_rgb)
+    {
+        $this->_red = $_red;
+        $this->_green = $_green;
+        $this->_blue = $_blue;
+        $this->_rgb = $_rgb;
+    }
+
+
+}
+?>
